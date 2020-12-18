c59fa58c2df3cf97bcf9551ba368b3b7ce97404c
chengqian <halo@fingo.group>
2020-12-18 12:06:30 +0800

    第一次提交

--- python/test_001.py
+++ python/test_001.py
@@ -1,9 +1,15 @@
-# -*- coding: utf-8 -*-
-import sys
+#-*- coding: UTF-8 -*-
+import time
+from selenium import webdriver
+def Test_selenium():
+    driver = webdriver.Chrome()
+    driver.get("http://www.baidu.com")
+    driver.find_element_by_id('kw').send_keys('一只老母猪')
+    driver.find_element_by_id('su').click()
+    time.sleep(5)
+    driver.quit()
 
 
-
-
-
-
+if __name__ == '__main__':
+    Test_selenium()
 
