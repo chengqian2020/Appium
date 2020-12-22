c6a127b28d5526532dc7cc51a4e50c7d8b504911
chengqian <halo@fingo.group>
2020-12-22 21:34:19 +0800

    再次犯错

--- seleniumcases/base.py
+++ seleniumcases/base.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 
 class Base():
     def setup(self):
-        self.driver = webdriver.chrome()
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(3)
         self.driver.maximize_window()
 
-    def tearduwn(self):
+    def teardown(self):
         self.driver.quit()
